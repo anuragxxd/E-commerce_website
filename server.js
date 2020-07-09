7c896a37098b7e89c2e76d65ff509b1b8a81c519
Anurag Gupta <aguptaking@gmail.com>
2020-07-09 13:43:51 +0530

    commit1

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const sellItemRouter = require("./src/routes/sellItems");
 const buyItemRouter = require("./src/routes/buyItems");
 const cookieParser = require("cookie-parser");
 const jwt = require("jsonwebtoken");
+const path = require("path");
 require("./src/db/mongoose");
 
 app.use(express.json());
@@ -12,6 +13,14 @@ app.use(userRouter);
 app.use(sellItemRouter);
 app.use(buyItemRouter);
 app.use(cookieParser());
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const port = process.env.PORT;
 
