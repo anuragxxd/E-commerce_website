9fb02f313a41e740f3f5464b8d4c9a5169e99279
Anurag Gupta <aguptaking@gmail.com>
2020-07-09 13:43:51 +0530

    commit1

---
+++ client/build/precache-manifest.bf2515b7a31d29f816ed64b39ddf10ed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab59866369d22d1a59c5e09a8eb632b8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ad792b088efd65c082b6",
+    "url": "/static/css/2.5a510433.chunk.css"
+  },
+  {
+    "revision": "f6762e8068244ecd9838",
+    "url": "/static/css/main.9ce77354.chunk.css"
+  },
+  {
+    "revision": "ad792b088efd65c082b6",
+    "url": "/static/js/2.c65b4776.chunk.js"
+  },
+  {
+    "revision": "f25112c0081db71b0e5dd2ce64e909e6",
+    "url": "/static/js/2.c65b4776.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6762e8068244ecd9838",
+    "url": "/static/js/main.ef54fdfd.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
